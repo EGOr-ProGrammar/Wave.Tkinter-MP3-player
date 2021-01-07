917c176ad02e2a5ad1aaf0b290573f54b2870c1d
Egorka <egor.learnmail@bk.ru>
2021-01-07 13:24:20 +0300

    Changed stop btn icon to pause btn. Added functions to stop, pause and unpause song.

--- main.py
+++ main.py
@@ -6,16 +6,18 @@ import pygame
 pygame.mixer.init()
 
 class Mp3Player():
-    # Ui and logic of a simple mp3 player
+    """ Ui and logic of a simple mp3 player"""
     def __init__(self, root):
-        # Intitiating all ui
+        """ Intitiating all ui, global variables"""
+
+        global paused
+        paused = False
 
         # Create playlist box
-        #bg="black", fg="green"
         self.__song_box = tk.Listbox(root, bg="black", fg="green", width="60", selectbackground="green", selectforeground="black")
 
         # Buttons icons
-        self.__stop_btn_img = tk.PhotoImage(file="images\stop50.png")
+        self.__pause_btn_img = tk.PhotoImage(file="images\pause50.png")
         self.__play_btn_img = tk.PhotoImage(file="images\play50.png")
         self.__forward_btn_img = tk.PhotoImage(file="images\\forward50.png")
         self.__back_btn_img = tk.PhotoImage(file="images\\back50.png")
@@ -25,8 +27,8 @@ class Mp3Player():
 
         # Controll buttons
         self.__back_btn = tk.Button(self.__btn_frame, image=self.__back_btn_img, borderwidth=0)
-        self.__play_btn = tk.Button(self.__btn_frame, image=self.__play_btn_img, borderwidth=0)
-        self.__stop_btn = tk.Button(self.__btn_frame, image=self.__stop_btn_img, borderwidth=0)
+        self.__play_btn = tk.Button(self.__btn_frame, image=self.__play_btn_img, borderwidth=0, command=self.play)
+        self.__pause_btn = tk.Button(self.__btn_frame, image=self.__pause_btn_img, borderwidth=0, command=self.pause)
         self.__forward_btn = tk.Button(self.__btn_frame, image=self.__forward_btn_img, borderwidth=0)
 
         # Create menu
@@ -44,11 +46,12 @@ class Mp3Player():
         self.__btn_frame.pack()
 
         self.__back_btn.grid(row=0, column=0, padx=10)
-        self.__stop_btn.grid(row=0, column=1, padx=10)
+        self.__pause_btn.grid(row=0, column=1, padx=10)
         self.__play_btn.grid(row=0, column=2, padx=10)
         self.__forward_btn.grid(row=0, column=3, padx=10)
 
     def add_song(self):
+        """ Adds a song to listbox deleting song's path and extension"""
         # You can change an 'initialdir' on your folder with music
         song = filedialog.askopenfilename(initialdir='audio/', title="Choose song", filetypes=(("mp3 Files", "*.mp3"), ))
 
@@ -58,6 +61,32 @@ class Mp3Player():
 
         # Add song to the end of listbox
         self.__song_box.insert(tk.END, song)
+
+    def play(self):
+        global paused
+        """ Plays selected song from listbox"""
+        song = self.__song_box.get(tk.ACTIVE)
+
+        song_path = f'D:\Wave\\audio\{song}.mp3'
+        try:
+            pygame.mixer.music.load(song_path)
+            pygame.mixer.music.play(loops=0)
+            paused = False
+        except:
+            pass
+
+    def pause(self):
+        """ Pause and unpause current song"""
+        global paused
+
+        # Unpause
+        if(paused):
+            pygame.mixer.music.unpause()
+            paused = False
+        # Pause
+        else:
+            pygame.mixer.music.pause()
+            paused = True 
 
         
     
